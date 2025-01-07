6140d1f04eed3c372a237a53943d84844fb055e5
sajidali12 <rizvisajid4@gmail.com>
2025-01-07 16:41:46 +0500

    first commit

---
+++ src/blocks/Counter/config.ts
@@ -0,0 +1,46 @@
+import type { Block } from 'payload'
+
+import {
+  FixedToolbarFeature,
+  HeadingFeature,
+  InlineToolbarFeature,
+  lexicalEditor,
+} from '@payloadcms/richtext-lexical'
+
+import { linkGroup } from '../../fields/linkGroup'
+
+export const Counters: Block = {
+  slug: 'counter',
+  interfaceName: 'CountersBlock',
+  fields: [
+    {
+      name: 'richText',
+      type: 'richText',
+      editor: lexicalEditor({
+        features: ({ rootFeatures }) => {
+          return [
+            ...rootFeatures,
+            HeadingFeature({ enabledHeadingSizes: ['h1', 'h2', 'h3', 'h4'] }),
+            FixedToolbarFeature(),
+            InlineToolbarFeature(),
+          ]
+        },
+      }),
+      label: false,
+    },
+    {
+      name: 'title',
+      type: 'text',
+    },
+    linkGroup({
+      appearances: ['default', 'outline'],
+      overrides: {
+        maxRows: 2,
+      },
+    }),
+  ],
+  labels: {
+    plural: 'Counters',
+    singular: 'Counter',
+  },
+}
