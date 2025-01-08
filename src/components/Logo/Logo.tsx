fd9d97ccd7e4643b2903870181f723df83637d7e
sajidali12 <rizvisajid4@gmail.com>
2025-01-08 11:25:30 +0500

    banner image

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -17,13 +17,13 @@ export const Logo = (props: Props) => {
     /* eslint-disable @next/next/no-img-element */
     <img
       alt="Payload Logo"
-      width={193}
-      height={34}
+      width={400}
+      height={70}
       loading={loading}
       fetchPriority={priority}
       decoding="async"
-      className={clsx('max-w-[9.375rem] w-full h-[34px]', className)}
-      src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-logo-light.svg"
+      className={clsx('max-w-[24rem] w-full h-[34px]', className)}
+      src="logo.svg"
     />
   )
 }
