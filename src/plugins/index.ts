41dd98287886e7ab9d3d8fe9ed5a5809c81a8c92
sajidali12 <rizvisajid4@gmail.com>
2025-01-08 14:12:01 +0500

    meta title

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -15,7 +15,9 @@ import { Page, Post } from '@/payload-types'
 import { getServerSideURL } from '@/utilities/getURL'
 
 const generateTitle: GenerateTitle<Post | Page> = ({ doc }) => {
-  return doc?.title ? `${doc.title} | Payload Website Template` : 'Payload Website Template'
+  return doc?.title
+    ? `${doc.title} | Connecting Pakistanis to America`
+    : 'Connecting Pakistanis to America'
 }
 
 const generateURL: GenerateURL<Post | Page> = ({ doc }) => {
